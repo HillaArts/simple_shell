54b790518eaf12ff5812daa6c6ea72256a6105d7
Isma3l-19 <ismaelnyambu.99@gmail.com>
2023-10-17 13:54:53 -0700

    putchar

---
+++ _betty.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+/**
+ * _putchar - prints _putchar
+ * @c: char
+ * Return: 0 (Success)
+ */
+int _putchar(char c)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
